2b4b93d0d061c00ea111bc3ac6d762cb12537a40
qq731574722 <731574722@qq.com>
2018-11-12 22:10:43 +0800

    添加项目文件。

---
+++ UWP/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace UWP
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            contentFrame.Navigate(typeof(HomePage));
+        }
+
+        private void nvSample_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (HomePageItem.IsSelected) { contentFrame.Navigate(typeof(HomePage)); }
+            else if (MinePageItem.IsSelected) { contentFrame.Navigate(typeof(MinePage)); }
+            else if (GetColorPageItem.IsSelected) { contentFrame.Navigate(typeof(GetColorPage)); }
+            else if (AboutPageItem.IsSelected) { contentFrame.Navigate(typeof(AboutPage)); }
+        }
+    }
+}
