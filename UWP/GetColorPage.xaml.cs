fa34f9da23e8a401c9febee48f56b16ac334274e
qq731574722 <731574722@qq.com>
2018-12-26 05:03:59 +0800

    可以获取初始颜色，取色器会根据图片大小改变自行移动

--- UWP/GetColorPage.xaml.cs
+++ UWP/GetColorPage.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using UWP.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -30,8 +31,9 @@ namespace UWP
     /// </summary>
     public sealed partial class GetColorPage : INotifyPropertyChanged
     {
-        private static WriteableBitmap image;
+        private WriteableBitmap image;
         private BitmapDecoder decoder;
+        
         private byte[] colorData;
         //private BitmapImage bi;
         public  WriteableBitmap Image
@@ -90,36 +92,108 @@ namespace UWP
                 image.SetSource(stream);
                 Img.Source = image;
                 colorData = pixelData.DetachPixelData();
-                //System.Diagnostics.Debug.WriteLine(colorData[0].ToString());
-                    
-                /*  自适应图片大小 */
-                if (image.PixelWidth <= 450)
-                {
-                    Img.MaxWidth = 450;
-                    Img.Stretch = Stretch.Uniform;
-                }
-                else if (image.PixelWidth < PickAreaGrid.RenderSize.Width)
-                {
-                    Img.MaxWidth = int.MaxValue;
-                    Img.Stretch = Stretch.None;
-                }
-                else
-                {
-                    Img.MaxWidth = int.MaxValue;
-                    Img.Stretch = Stretch.Uniform;
-                }
-                //Color0 = GetPosRGB(Canvas.GetLeft(Picker0)+12, Canvas.GetTop(Picker0)+30);
-                
-                //Color0 = GetPosRGB(32.0, 50.0);
-                //DragDeltaEventArgs dragDeltaEventArgs = new DragDeltaEventArgs(0, 0);
-                //Thumb_DragDelta(Picker0, dragDeltaEventArgs);
+
                 ReSelectImageButton.Visibility = Visibility.Visible;
                 SelectImageButton.Visibility = Visibility.Collapsed;
             }
         }
+      
+        private void Thumb_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            card.Colors[0].RGB = Color0;
+            card.Colors[1].RGB = Color1;
+            card.Colors[2].RGB = Color2;
+            card.Colors[3].RGB = Color3;
+        }
 
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            UIElement thumb = (UIElement)sender;
+            double posX = Canvas.GetLeft(thumb) + e.HorizontalChange;
+            double posY = Canvas.GetTop(thumb) + e.VerticalChange;
+            if (posX >= 0 && posX <= PickAreaGrid.RenderSize.Width - 24)
+                Canvas.SetLeft(thumb, posX);
+            if (posY >= 0 && posY <= PickAreaGrid.RenderSize.Height - 30)
+                Canvas.SetTop(thumb, posY);
+            if (thumb.Equals(Picker0))
+                Color0 = GetPosRGB(posX + 12, posY + 30);
+            if (thumb.Equals(Picker1))
+                Color1 = GetPosRGB(posX + 12, posY + 30);
+            if (thumb.Equals(Picker2))
+                Color2 = GetPosRGB(posX + 12, posY + 30);
+            if (thumb.Equals(Picker3))
+                Color3 = GetPosRGB(posX + 12, posY + 30);
+        }
 
+        private void Img_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(PickAreaGrid.RenderSize.Width + "  " + image.PixelWidth);
+            /*  自适应图片大小 */
+            if (image.PixelWidth <= 450)
+            {
+                Img.MaxWidth = 450;
+                Img.Stretch = Stretch.Uniform;
+            }
+            else if (image.PixelWidth < PickAreaGrid.RenderSize.Width)
+            {
+                Img.MaxWidth = int.MaxValue;
+                Img.Stretch = Stretch.None;
+            }
+            else
+            {
+                Img.MaxWidth = int.MaxValue;
+                Img.Stretch = Stretch.Uniform;
+            }
 
+            Color0 = GetPickerRGB(Picker0);
+            Color1 = GetPickerRGB(Picker1);
+            Color2 = GetPickerRGB(Picker2);
+            Color3 = GetPickerRGB(Picker3);
+            PosChanged(Picker0, e);
+            PosChanged(Picker1, e);
+            PosChanged(Picker2, e);
+            PosChanged(Picker3, e);
+        }
+
+        private void PosChanged(Thumb picker, SizeChangedEventArgs e)
+        {
+            
+            if (e.PreviousSize.Width == 0) return;
+            double scaleX = e.NewSize.Width / e.PreviousSize.Width;
+            double scaleY = e.NewSize.Height / e.PreviousSize.Height;
+            double PreviousPosX = Canvas.GetLeft(picker);
+            double PreviousPosY = Canvas.GetTop(picker);
+            double newPosX = PreviousPosX * scaleX;
+            double newPoxY = PreviousPosY * scaleY;
+            DragDeltaEventArgs dragDeltaEventArgs = new DragDeltaEventArgs(newPosX - PreviousPosX, newPoxY - PreviousPosY);
+            Thumb_DragDelta(picker, dragDeltaEventArgs);
+        }
+
+        /* 计算缩放比将坐标转换为图片上的像素 */
+        private int ConvertPosToPixel(double pos)
+        {
+            double scale = image.PixelWidth / PickAreaGrid.RenderSize.Width;
+            return (int)(pos * scale);
+        }
+        private string ARGB2RGB(string ARGB)
+        {
+            string RGB = "#" + ARGB.Substring(3);
+            return RGB;
+        }
+        private string GetPosRGB(double posX,double posY)
+        {
+            int pixelX = ConvertPosToPixel((int)posX);
+            int pixelY = ConvertPosToPixel((int)posY);
+            var k = (pixelY * decoder.PixelWidth + pixelX) * 4;
+            Windows.UI.Color color=new Windows.UI.Color();
+            if (k+4<=colorData.Length)
+                color = Windows.UI.Color.FromArgb(colorData[k + 3], colorData[k + 2], colorData[k + 1], colorData[k + 0]);
+            return ARGB2RGB(color.ToString());
+        }
+        private string GetPickerRGB(Thumb picker)
+        {
+            return GetPosRGB(Canvas.GetLeft(picker) + 12, Canvas.GetTop(picker) + 30);
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -177,51 +251,5 @@ namespace UWP
             }
         }
 
-        private void Thumb_DragCompleted(object sender, DragCompletedEventArgs e)
-        {
-            card.Colors[0].RGB = Color0;
-        }
-
-        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
-        {
-            UIElement thumb = (UIElement)sender;
-            double posX = Canvas.GetLeft(thumb) + e.HorizontalChange;
-            double posY = Canvas.GetTop(thumb) + e.VerticalChange;
-            if (posX >= 0 && posX <= PickAreaGrid.RenderSize.Width - 24)
-                Canvas.SetLeft(thumb, posX);
-            if (posY >= 0 && posY <= PickAreaGrid.RenderSize.Height - 30)
-                Canvas.SetTop(thumb, posY);
-
-            if (thumb.Equals(Picker0))
-                Color0 = GetPosRGB(posX + 12, posY + 30);
-            if (thumb.Equals(Picker1))
-                Color1 = GetPosRGB(posX + 12, posY + 30);
-            if (thumb.Equals(Picker2))
-                Color2 = GetPosRGB(posX + 12, posY + 30);
-            if (thumb.Equals(Picker3))
-                Color3 = GetPosRGB(posX + 12, posY + 30);
-        }
-
-        /* 计算缩放比将坐标转换为图片上的像素 */
-        private int ConvertPosToPixel(double pos)
-        {
-            double scale = image.PixelWidth / PickAreaGrid.RenderSize.Width;
-            return (int)(pos * scale);
-        }
-        private string ARGB2RGB(string ARGB)
-        {
-            string RGB = "#" + ARGB.Substring(3);
-            return RGB;
-        }
-        private string GetPosRGB(double posX,double posY)
-        {
-            int pixelX = ConvertPosToPixel((int)posX);
-            int pixelY = ConvertPosToPixel((int)posY);
-            var k = (pixelY * decoder.PixelWidth + pixelX) * 4;
-            Windows.UI.Color color=new Windows.UI.Color();
-            if (k+4<=colorData.Length)
-                color = Windows.UI.Color.FromArgb(colorData[k + 3], colorData[k + 2], colorData[k + 1], colorData[k + 0]);
-            return ARGB2RGB(color.ToString());
-        }
     }
 }
